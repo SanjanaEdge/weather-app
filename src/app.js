e687f5fc675efdb3a5774928e6b59c68ca3959af
SanjanaEdge <sanjanaedge@gmail.com>
2022-04-25 18:56:35 -0400

    added the search engine

--- src/app.js
+++ src/app.js
@@ -48,12 +48,23 @@ function displayTemperature(response) {
   
 }
 
-
-let apiKey = "5ce3ac747e47d7c5202a01517c1610db";
-let city = "Toronto";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+function search(city) {
+  let apiKey = "5ce3ac747e47d7c5202a01517c1610db";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayTemperature);
+}
   
-axios.get(apiUrl).then(displayTemperature);
+
+function handleSubmit(event) {
+  event.preventDefault();
+  let cityInputElement = document.querySelector("#city-input");
+  search(cityInputElement.value);
+}
+
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", handleSubmit);
+
+search("Toronto");
 
 
 
